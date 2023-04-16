f94471b7ee4ed60db31961d444dd748e747f60f5
OutisDeveloper <behruz234o@gmail.com>
2023-04-16 21:32:16 +0500

    carousel without responsibility

--- js/main.js
+++ js/main.js
@@ -37,3 +37,20 @@ setTimeout(() => {
   loading.classList.add("loading-none");
 }, 1000);
 // loading
+
+// owl-carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    autoplay: false,
+    margin: 30,
+    loop: true,
+    nav: true,
+    navText: [
+      "<i class='fas fa-long-arrow-alt-left'></i>",
+      "<i class='fas fa-long-arrow-alt-right'></i>",
+    ],
+  });
+});
+
+// owl-carousel
